695c71e57b5a364930dccb08f0c94c8414dbc97b
David Janes <davidjanes@davidjanes.com>
2015-03-29 10:06:40 -0400

    make sure model files are distributed

--- DIST.sh
+++ DIST.sh
@@ -31,7 +31,8 @@ echo "=================="
 
     tar cf - \
         README.md LICENSE homestar.json package.json \
-        *.js \
+        DenonAVRBridge.js index.js \
+        models/*js \
         |
     ( cd "${NPM_DST}" && tar xvf - )
 
