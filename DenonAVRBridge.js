310266fdd9f4f7c7699c27f0545a605dcea3684b
David Janes <davidjanes@davidjanes.com>
2015-04-25 10:49:15 -0400

    remove old documentation dups

--- DenonAVRBridge.js
+++ DenonAVRBridge.js
@@ -41,16 +41,10 @@ var logger = bunyan.createLogger({
 });
 
 /**
- *  EXEMPLAR and INSTANCE
+ *  See {iotdb.bridge.Bridge#Bridge} for documentation.
  *  <p>
- *  No subclassing needed! The following functions are
- *  injected _after_ this is created, and before .discover and .connect
- *  <ul>
- *  <li><code>discovered</code> - tell IOTDB that we're talking to a new Thing
- *  <li><code>pulled</code> - got new data
- *  <li><code>connected</code> - this is connected to a Thing
- *  <li><code>disconnnected</code> - this has been disconnected from a Thing
- *  </ul>
+ *  @param {object|undefined} native
+ *  only used for instances, should be 
  */
 var DenonAVRBridge = function (initd, native) {
     var self = this;
@@ -84,13 +78,7 @@ DenonAVRBridge.prototype.name = function () {
 /* --- lifecycle --- */
 
 /**
- *  EXEMPLAR.
- *  Discover one or more Things.
- *  <ul>
- *  <li>look for Things (using <code>self.bridge</code> data to initialize)
- *  <li>find / create a <code>paramd</code> that does the talking
- *  <li>create an DenonAVRBridge(paramd)
- *  <li>call <code>self.discovered(bridge)</code> with it
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.discover = function () {
     var self = this;
@@ -176,8 +164,7 @@ DenonAVRBridge.prototype._discover_amdns = function () {
 };
 
 /**
- *  INTERNAL EXEMPLAR
- *  Connect to a particular host
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype._discover_host = function (discoverd) {
     var self = this;
@@ -320,8 +307,7 @@ DenonAVRBridge.prototype._discover_host = function (discoverd) {
 };
 
 /**
- *  INSTANCE
- *  Call when ready to be used as an instance
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.connect = function (connectd) {
     var self = this;
@@ -459,8 +445,7 @@ DenonAVRBridge.prototype._received = function (message) {
 };
 
 /**
- *  INSTANCE and EXEMPLAR (during shutdown).
- *  This is called when the Bridge is no longer needed. When
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.disconnect = function () {
     var self = this;
@@ -475,8 +460,7 @@ DenonAVRBridge.prototype.disconnect = function () {
 /* --- data --- */
 
 /**
- *  INSTANCE.
- *  Send data to whatever you're taking to.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.push = function (pushd) {
     var self = this;
@@ -544,9 +528,7 @@ DenonAVRBridge.prototype.push = function (pushd) {
 };
 
 /**
- *  INSTANCE.
- *  Pull data from whatever we're talking to. You don't
- *  have to implement this if it doesn't make sense
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.pull = function () {
     var self = this;
@@ -567,17 +549,7 @@ DenonAVRBridge.prototype.pull = function () {
 /* --- state --- */
 
 /**
- *  INSTANCE.
- *  Return the metadata - compact form can be used.
- *  <p>
- *  Really really useful things are:
- *  <ul>
- *  <li><code>iot:thing</code> required - a unique ID
- *  <li><code>iot:device</code> suggested if linking multiple things together
- *  <li><code>schema:name</code>
- *  <li><code>iot:number</code>
- *  <li><code>schema:manufacturer</code>
- *  <li><code>schema:model</code>
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.meta = function () {
     var self = this;
@@ -590,20 +562,14 @@ DenonAVRBridge.prototype.meta = function () {
 };
 
 /**
- *  INSTANCE.
- *  Return True if this is reachable. You
- *  do not need to worry about connect / disconnect /
- *  shutdown states, they will be always checked first.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.reachable = function () {
     return this.native !== null;
 };
 
 /**
- *  INSTANCE.
- *  Configure an express web page to configure this Bridge.
- *  Return the name of the Bridge, which may be
- *  listed and displayed to the user.
+ *  See {iotdb.bridge.Bridge#XXX} for documentation.
  */
 DenonAVRBridge.prototype.configure = function (app) {};
 
