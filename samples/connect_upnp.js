91d50ed248171bb587b4dae264b202f85fbeee17
David Janes <davidjanes@davidjanes.com>
2015-12-26 09:15:00 -0330

    IoTQL models

---
+++ samples/connect_upnp.js
@@ -0,0 +1,23 @@
+/*
+ *  NOTE: prefer to do the way 'model.js' works
+ *  Connect to a Denon AVR by searching MDNS/Bonjour
+ */
+
+"use strict";
+
+var DenonAVRBridge = require('../DenonAVRBridge').Bridge;
+
+var denon = new DenonAVRBridge({
+    upnp: true,
+});
+denon.discovered = function (bridge) {
+    console.log("+ got one", bridge.meta());
+    bridge.pulled = function (state) {
+        console.log("+ state-change", state);
+    };
+    bridge.connect({});
+    bridge.push({
+        volume: 0.5,
+    }, function() {});
+};
+denon.discover();
