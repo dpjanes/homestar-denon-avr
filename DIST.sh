19b1e75ec736e05e75e0c21a3419acbfb4aeea7f
David Janes <davidjanes@davidjanes.com>
2015-12-26 12:38:48 -0330

    auto commit/push package.json

--- DIST.sh
+++ DIST.sh
@@ -34,12 +34,10 @@ echo "=================="
         DenonAVRBridge.js index.js \
         models/*.js models/*.json \
         |
-    ( cd "${NPM_DST}" && tar xvf - )
+    ( cd "${NPM_DST}" && tar xvf - && npm publish ) || exit 1
 
-    ## cp dist/*.* "${NPM_DST}" || exit 1
-
-    cd "${NPM_DST}" || exit 1
-    npm publish
+    git commit -m "new release" package.json || exit 1
+    git push || exit 1
 
     echo "end"
 )
